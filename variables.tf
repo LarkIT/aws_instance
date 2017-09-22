ab67940dda3f83fea4d94dfce9630b207d7278f2
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-09-22 13:26:40 -0600

    refactor code

--- variables.tf
+++ variables.tf
@@ -9,3 +9,13 @@ variable "host_prefix" {
 variable "internal_domain_name" {
   description = "Hostname prefix (abc)"
 }
+
+variable "bootscript_script" {
+  description = "Shell script to bootstrap the system."
+  default     = "bootstrap.sh"
+}
+
+variable "reboot_script" {
+  description = "Shell script to reboot the system."
+  default     = "reboot.sh"
+}
