1f76839e17194c6048236d3f20ce37bea80e11ba
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-10-03 11:20:57 -0600

    minor

--- foreman.tf
+++ foreman.tf
@@ -1,13 +1,13 @@
-data "template_file" "foreman" {
-  template = "${file("${path.module}/templates/foreman-install.sh.tpl")}"
-  vars {
-    hostname      = "${var.host_prefix}-foreman-01.${var.internal_domain_name}"
-    puppet_server = "${var.host_prefix}-foreman-01.${var.internal_domain_name}"
-    gitlab_server = "${var.host_prefix}-gitlab-01.${var.internal_domain_name}"
-    puppet_env    = "production"
-    role          = "${var.role}"
-  }
-}
+#data "template_file" "foreman" {
+#  template = "${file("${path.module}/templates/foreman-install.sh.tpl")}"
+#  vars {
+#    hostname      = "${var.host_prefix}-foreman-01.${var.internal_domain_name}"
+#    puppet_server = "${var.host_prefix}-foreman-01.${var.internal_domain_name}"
+#    gitlab_server = "${var.host_prefix}-gitlab-01.${var.internal_domain_name}"
+#    puppet_env    = "production"
+#    role          = "${var.role}"
+#  }
+#}
 
 #provisioner "file" {
 #  content = "${data.template_file.foreman.rendered}"
@@ -15,10 +15,10 @@ data "template_file" "foreman" {
 #}
 
 
-data "template_cloudinit_config" "foreman" {
-  part {
-    filename = "foreman-install.sh"
-    content_type = "text/x-shellscript"
-    content = "${data.template_file.foreman.rendered}"
-  }
-}
+#data "template_cloudinit_config" "foreman" {
+#  part {
+#    filename = "foreman-install.sh"
+#    content_type = "text/x-shellscript"
+#    content = "${data.template_file.foreman.rendered}"
+#  }
+#}
