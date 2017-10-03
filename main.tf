a1240a4f0fc8b452f644945712d883f02e311278
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-10-03 01:09:50 -0600

    minor

--- main.tf
+++ main.tf
@@ -10,7 +10,9 @@ resource "aws_instance" "hostname" {
     vpc_security_group_ids = [ "${var.general_id}" ]
     subnet_id              = "${var.subnet_id}"
 #    user_data              = "${data.template_cloudinit_config.hostname.rendered}"
-    user_data              = "${replace(data.template_cloudinit_config.role.rendered, "role", var.role)}"
+#    user_data              = "${file(var.user_data)}"
+    user_data              = "${data.template_file.foreman.rendered}"
+#    user_data              = "${replace(data.template_cloudinit_config.role.rendered, "role", var.role)}"
 #    user_data              = "${data.template_cloudinit_config.foreman.rendered}"
 #    user_data              = "${element(data.template_cloudinit_config.*.rendered, var.role)}"
 #    user_data               = "${element(data.template_cloudinit_config.*.rendered, count.index)}"
