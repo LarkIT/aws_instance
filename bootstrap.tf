f8f12e31af338620a233e3c058c2889df7ce47a8
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-10-02 22:36:17 -0600

    minor commit

--- bootstrap.tf
+++ bootstrap.tf
@@ -9,10 +9,26 @@ data "template_file" "bootstrap" {
   }
 }
 
-#data "template_file" "foreman-install" {
-#  template = "${file("${path.module}/templates/foreman-install.sh.tpl")}"
-#  vars     = "${data.template_file.bootstrap-foreman-01.vars}"
-#}
+data "template_file" "bootstrap-foreman-01" {
+  template = "${file("${path.module}/templates/bootstrap.sh")}"
+  vars {
+#    ad_domain = "${var.ad_domain_name}"
+#    ad_shortname = "${var.short_name}"
+#    joiner_account = "${var.joiner_account}"
+#    joiner_pw = "${var.joiner_password}"
+#    ad_dns = "['${join("','", aws_directory_service_directory.dsdomain.dns_ip_addresses)}']"
+    hostname = "${var.host_prefix}-foreman-01.${var.ad_domain_name}"
+    puppet_server = "${var.host_prefix}-foreman-01.${var.ad_domain_name}"
+    gitlab_server = "${var.host_prefix}-gitlab-01.${var.ad_domain_name}"
+    puppet_env = "production"
+    role = "foreman"
+  }
+}
+
+data "template_file" "foreman-install" {
+  template = "${file("${path.module}/templates/foreman-install.sh.tpl")}"
+  vars     = "${data.template_file.hostname.vars}"
+}
 
 data "template_cloudinit_config" "hostname" {
   part {
