ace08fe7ee155cc839211ad8a3d4a7a6a3ed4605
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-11-29 15:36:02 -0700

    test

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "hostname" {
     iam_instance_profile   = "${var.iam_instance_profile}"
 
     lifecycle {
-      ignore_changes = ["user_data", "ami"]
+      ignore_changes = ["user_data"]
     }
 
     tags {
