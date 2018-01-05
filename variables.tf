ef981158b4fce088eb7f7dbcec9700677f309bb5
Nicolas Fosdick <nick.fosdick@sigfig.com>
2018-01-05 14:24:24 -0700

    Reverting Andrews pulp changes

--- variables.tf
+++ variables.tf
@@ -128,17 +128,11 @@ variable "ebs_volume_size" {
 }
 
 variable "device_name" {
-  description = "Filesystem path for EBS block device"
+  description = "The name of the ebs device"
   default     = "/dev/xvdf"
-}
-
-variable "pulp_device_name" {
-  description = "Filesystem path for Pulp block device"
-  default = "/dev/xvdg"
 }
 
 variable "bootstrap_template" {
   description = "Custom bootstrap template"
   default     = "blank"
 }
-
