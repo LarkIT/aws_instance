a705b402806417c38835708480426757ed41845a
Nicolas Fosdick <nick.fosdick@sigfig.com>
2018-01-19 10:51:17 -0700

    Initial commit

---
+++ main.tf
@@ -0,0 +1,83 @@
+module "bootstrap" {
+  source               = "git::https://nfosdick@bitbucket.org/larkit/bootstrap.git"
+  internal_domain_name = "${var.internal_domain_name}"
+  host_prefix          = "${var.host_prefix}"
+  role                 = "${var.role}"
+  pp_env               = "${var.pp_env}"
+  hostname             = "${var.hostname}"
+  region               = "${var.region}" 
+  bootstrap_template   = "${var.bootstrap_template}"
+}
+
+resource "aws_instance" "hostname" {
+    ami                    = "${lookup(var.centos7-ami, var.region)}"
+    availability_zone      = "${var.region}${var.availability_zone}"
+    instance_type          = "${var.instance_type}"
+    key_name               = "${var.aws_key_name}"
+    vpc_security_group_ids = [ "${var.security_groups}" ]
+    subnet_id              = "${var.subnet_id}"
+    user_data              = "${module.bootstrap.base_cloutinit}"
+    iam_instance_profile   = "${var.iam_instance_profile}"
+
+    lifecycle {
+      ignore_changes = ["user_data"]
+    }
+
+    tags {
+        Name = "${var.host_prefix}-${var.hostname}"
+    }
+}
+
+resource "aws_route53_record" "hostname" {
+  zone_id = "${var.route53_internal_id}"
+  name    = "${aws_instance.hostname.tags.Name}"
+  type    = "A"
+  ttl     = "300"
+  records = ["${aws_instance.hostname.private_ip}"]
+}
+
+#output "hostname" {
+#  value = "${aws_route53_record.hostname.name} (${aws_instance.hostname.private_ip})\t[${aws_eip.hostname.public_ip}]"
+#}
+
+resource "aws_eip" "hostname" {
+    count    = "${var.enable_aws_eip}"
+    instance = "${aws_instance.hostname.id}"
+    vpc      = true
+}
+
+resource "aws_route53_record" "hostname-ext" {
+  count   = "${var.external_dns_enable}"
+  zone_id = "${var.route53_external_id}"
+  #name    = "${aws_instance.hostname.tags.Name}"
+  name    = "${var.external_hostname}"
+  type    = "A"
+  ttl     = "300"
+  records = ["${aws_eip.hostname.public_ip}"]
+}
+
+resource "aws_ebs_volume" "volume" {
+  count             = "${var.enable_ebs_volume}"
+  availability_zone = "${var.region}${var.availability_zone}"
+  size              = "${var.ebs_volume_size}"
+  type              = "${var.ebs_type}"
+  tags {
+    Name = "${var.role} Volume for ${var.host_prefix}-${var.hostname}"
+  }
+}
+
+resource "aws_volume_attachment" "ebs_att" {
+  count       = "${var.enable_ebs_volume}"
+  device_name = "${var.device_name}"
+  volume_id   = "${aws_ebs_volume.volume.id}"
+  instance_id = "${aws_instance.hostname.id}"
+}
+
+#output "hostname-ext" {
+#  value = "${aws_route53_record.hostname-ext.fqdn} (${aws_eip.hostname.public_ip})"
+#}
+
+# Special SSH Command for JumpHost
+#output "hostname-ssh-command" {
+#  value = "ssh -A centos@${aws_eip.hostname.public_ip} -L 9443:${aws_instance.gitlab-01.tags.Name}:443 -L 2222:${aws_instance.gitlab-01.tags.Name}:22 -L 8443:${aws_instance.foreman-01.tags.Name}:443 "
+#}
---
+++ variables.tf
@@ -0,0 +1,138 @@
+variable "region" {
+  description = "The AWS region."
+}
+
+variable "hostname" {
+  description = "The AWS hostname assigned to the server."
+}
+
+variable "host_prefix" {
+  description = "Hostname prefix (abc)"
+}
+
+variable "internal_domain_name" {
+  description = "Hostname prefix (abc)"
+}
+
+variable "bootscript_script" {
+  description = "Shell script to bootstrap the system."
+  default     = "bootstrap.sh"
+}
+
+variable "reboot_script" {
+  description = "Shell script to reboot the system."
+  default     = "reboot.sh"
+}
+
+variable "instance_type" {
+  description = "Virtual machine tshirt size"
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "Virtual machine aws ami"
+  default     = "ami-f5d7f195"
+}
+
+variable "centos7-ami"{
+  description = "AMIs by region"
+  default = {
+    us-east-1 = "ami-46c1b650"
+    us-east-2 = "ami-18f8df7d"
+    us-west-1 = "ami-f5d7f195"
+    us-west-2 = "ami-f4533694"
+  }
+}
+
+variable "availability_zone" {
+  description = "VPC network availability zone"
+  default     = "a"
+}
+
+variable "aws_key_name" {
+  description = "SSH Public Key Name in AWS"
+  default = "lark-provisioning"
+}
+
+variable "security_groups" {
+  description = "SSH Public Key Name in AWS"
+  type        = "list"
+}
+
+variable "subnet_id" {
+  description = "SSH Public Key Name in AWS"
+}
+
+variable "iam_instance_profile" {
+  description = "SSH Public Key Name in AWS"
+  default     = "basicServer"
+}
+
+variable "route53_internal_id" {
+  description = "SSH Public Key Name in AWS"
+}
+
+variable "route53_external_id" {
+  description = "SSH Public Key Name in AWS"
+  default     = ""
+}
+
+variable "external_dns_enable" {
+  description = "Enable an external dns on hostname"
+  default     = false
+}
+
+variable "external_hostname" {
+  description = "Enable an external dns on hostname"
+  default     = "aws.lark-it.com"
+}
+
+variable "number_of_instances" {
+  description = "Number of virutal machines to create."
+  default     = "1"
+}
+
+variable "role" {
+  description = "Puppet classification role"
+  default     = "base"
+}
+
+variable "pp_env" {
+  description = "Puppet environment"
+  default     = "production"
+}
+
+#variable "bootstrap" {
+#  description = "Puppet classification role"
+#  default     = "${module.bootstrap.base_cloutinit}"
+#}
+
+variable "enable_aws_eip" {
+  description = "External IP to connect to"
+  default     = false
+}
+
+variable "enable_ebs_volume" {
+  description = "Additional ebs volume"
+  default     = false
+}
+
+variable "ebs_type" {
+  description = "The type of EBS volume."
+  default     = "gp2"
+}
+
+variable "ebs_volume_size" {
+  description = "Size of the ebs volume"
+  default     = "30"
+}
+
+variable "device_name" {
+  description = "The name of the ebs device"
+  default     = "/dev/xvdf"
+}
+
+variable "bootstrap_template" {
+  description = "Custom bootstrap template"
+  default     = "blank"
+}
