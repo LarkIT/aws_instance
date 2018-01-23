6fde9eb66f611dbf8b6e0a680ccddeb4d7157e09
Andrew Alles <andrew@lark-it.com>
2018-01-23 15:30:42 -0700

    Whoops - pointing a module to itself does weird things

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "bootstrap" {
-  source               = "git::https://github.com/LarkIT/aws_instance.git?ref=aspyrchanges"
+  source               = "git::https://github.com/LarkIT/bootstrap.git?ref=aspyrchanges"
   internal_domain_name = "${var.internal_domain_name}"
   host_prefix          = "${var.host_prefix}"
   role                 = "${var.role}"
